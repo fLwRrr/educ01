1ba5bc08cf4011421eab465c64403a961b9db66b
fLwRrr <myduckyduck@gmail.com>
2017-08-28 17:24:13 +0300

    Пишем автоматизацию!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,54 @@
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks("grunt-sass");
+  require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
     sass: {
-      newConfig: {
-        files: {
+      style: {
+        files: [
           "css/style.css":"scss/style.scss"
+        ]
+      }
+    },
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")({browsers: [
+            "last 1 version",
+            "last 2 Chrome versions",
+            "last 2 Firefox versions".
+            "last 2 Opera versions".
+            "last 2 Edge versions"
+          ]})
+        ]
+      },
+      style: {
+        src: "css/*.css"
+      }
+    },
+    watch: {
+      style: {
+        files: [
+          "scss/**/*.scss"
+        ],
+        tasks: [
+          "sass", "postcss"
+        ]
+      }
+    },
+    browserSync: {
+      server {
+        bsFiles: {
+          src: [
+            "*.html", "css/*.css"
+          ]
+        },
+        options: {
+          server: "."
         }
       }
     }
+
   });
 
 };
